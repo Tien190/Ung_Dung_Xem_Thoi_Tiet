8a6276dbb9ded3c781109571cd5d1cb187ae88d3
Tien190 <hvt1230@gmail.com>
2026-01-20 21:21:01 +0700

    purge: empty main branch content

--- backend/src/config.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-OPENWEATHER_API_KEY = os.getenv("OPENWEATHER_API_KEY")
-
-BASE_WEATHER_URL = "https://api.openweathermap.org/data/2.5"
-GEOCODING_URL = "https://api.openweathermap.org/geo/1.0"
--- backend/src/models/data_models.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel
-
-class CityResult(BaseModel):
-    name: str
-    lat: float
-    lon: float
